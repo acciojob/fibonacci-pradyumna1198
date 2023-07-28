b51dc42b3abbf82ad0c31e0813cfb8eb2a1180f8
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-07-28 13:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function fibonacci(num) {
 // your code here
-	const n = 0;
-	if(num <= 1)
-	{
-		return num;
-	}
+	let n = 0;
+	
 	for(let i=0;i<=50;i++){
-	return fibonacci(num-1) + fibonacci(num-2);}
+
+		if(num <= 1)
+		{
+			return num;
+		}
+	return fibonacci(num - 1) + fibonacci(num - 2);}
 }
 
 module.exports = fibonacci;
